648ad98217846d996e4fc400f72e572cf497af08
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-09-20 11:03:12 -0600

    intern finished

---
+++ tests/intern.test.js
@@ -0,0 +1,22 @@
+const Intern = require("../lib/Intern");
+
+// -done
+test("will set school username via constructor", () => {
+  const testValue = "UD";
+  const e = new Intern("Carla", 1, "email@email.com", testValue);
+  expect(e.school).toBe("testValue)");
+});
+
+// getSchool()-done
+test("will set school username via getSchool()", () => {
+  const testValue = "UD";
+  const e = new Intern("Carla", 1, "email@email.com", testValue);
+  expect(e.getSchool()).toBe("testValue)");
+});
+
+// getRole()-done
+test("it will return /Intern via getRole()", () => {
+  const testValue = "Intern";
+  const e = new Intern("Carla", 1, "email@email.com", "UD");
+  expect(e.getRole()).toBe(testValue);
+});
