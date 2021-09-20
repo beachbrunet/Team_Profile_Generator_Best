1e0463ad7daaf6d447422380d5c9115ea6a68f2b
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-09-20 10:43:31 -0600

    working on tests, did employee and engineer

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,9 +1,41 @@
 const Employee = require("../lib/Employee");
 
-describe("Employee", () => {
-  it("it will return a name when created", () => {
-    const name = "Carla";
-    const object = new Employee(testName, 1, "email@email.com");
-    expect(object.name).toEqual(testName);
-  });
+test("it will ", () => {
+  const e = new Employee();
+  expect(type(e)).toBe("Object");
+});
+
+test("it will ", () => {
+  const e = new Employee();
+  expect(type(e)).toBe("Object");
+});
+
+// getName(), getId(), getEmail(), getRole()—returns 'Employee'
+
+// getName()
+test("it will get a name via getName() ", () => {
+  const testValue = "Carla";
+  const e = new Employee(testValue);
+  expect(e.getName()).toBe("testValue)");
+});
+
+// getId
+test("it will get a name via getId() ", () => {
+  const testValue = "100";
+  const e = new Employee("blah", testValue);
+  expect(e.getId()).toBe("testValue)");
+});
+
+// getEmail()
+test("it will get a name via getName() ", () => {
+  const testValue = "email@email.com";
+  const e = new Employee("blah", 1, testValue);
+  expect(e.getEmail()).toBe("testValue)");
+});
+
+// getRole
+test("it will get a name via getName() ", () => {
+  const testValue = "Employee";
+  const e = new Employee("Carla", 1, "email@email.com");
+  expect(e.getName()).toBe("testValue)");
 });
